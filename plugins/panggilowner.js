95cd6f84c349da271bedb8155242f47d4de9d683
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-16 12:14:53 +0800

    Update panggilowner.js

--- plugins/panggilowner.js
+++ plugins/panggilowner.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./src/tag.webp')
-conn.fakeReply(m.chat, stc, '60189830350@s.whatsapp.net', '*Kenapa Tag Owner Saya*')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Kenapa Tag Owner Saya*')
 }
 handler.customPrefix = /^(@60189830350)$/i
 handler.command = new RegExp
