c6993d04247ee0ea2f6b1349d3f5faf287d4044f
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2022-03-21 09:45:45 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-Hah sc? Desah dlu deck
-`.trim(), '©LynXzy🥀', 'Iya bang\n\ntuh cek desk grub\nhttps://chat.whatsapp.com/EheAWPrQMhV25xW0N7l7WD', 'canda')
+─❉ [ 𝙎𝙘 𝙍𝙚𝙘𝙤𝙙𝙚 ] ❉─
+https://github.com/LynnXzy/RadBot-1
+─❉ [ 𝙎𝙘 𝙤𝙧𝙞 ] ❉─
+https://github.com/ikkygganzBotz/RadBot
+`.trim(), '©LynXzy🥀', 'Iya bang\n\njoin gc gw\nhttps://chat.whatsapp.com/EheAWPrQMhV25xW0N7l7WD', 'thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
